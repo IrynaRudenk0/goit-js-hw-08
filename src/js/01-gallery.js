96ee9de43746cf1ad3f2d26c560cfc0339b95ff5
Iryna Rudenko <irinka.rude@gmail.com>
2023-06-02 22:19:40 +0200

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+function makeGallery(items) {
+  return galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</li>`
+  )
+      .join("")
+
+  }
+const galleryList = document.querySelector('.gallery');
+const galleryCards = makeGallery(galleryItems);
+
+galleryList.innerHTML = galleryCards; 
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captionSelector:'img',
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionDelay: 100,
+ });
 
 console.log(galleryItems);
